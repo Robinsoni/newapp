ccf449c2dcd8211c36272d7042e490dcf386fa22
robin <sonirobin369@gmail.com>
2020-08-29 22:57:07 +0530

    Revert "First Hook used"
    This reverts commit 8e902c9daec7dfaecf120ebfc08c4e26f01c90fd.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,40 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'; // should be upper case as Person
-const app = (props) => {
-  const [currentState, setCurrentState] = useState({
-    persons:[
-      {name:'raghav',age:25},
-      {name:'sachin',age:23},
-    ]
-  })
-  const [otherState, setOtherState] = useState("This is the other state")
-  const swithNameHandler = () =>{
-    setCurrentState({
+class App extends Component {
+  state = {
+      persons:[
+        {name:'raghav',age:25},
+        {name:'sachin',age:23},
+      ]
+  }
+  swithNameHandler = () => {
+    //this.state.persons[0].name = "Robin" // this is not allowed you can not update the state directly
+    this.setState({
       persons:[
         {name:'Robin',age:25},
-        {name:'Sachin',age:23},
+        {name:'Sachin',age:23}
       ]
     })
+    console.log('It was cliced');
   }
-  console.log(currentState, otherState);
-  
+  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react</h1>
         <p>i'm react app and using the jsx</p>
-        <button onClick = {swithNameHandler}>switch button</button>
-        <Person name = {currentState.persons[0].name} age={currentState.persons[0].age}></Person>
+        <button onClick = {this.swithNameHandler}>switch button</button>
+        <Person name = {this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         <Person name = "Sachin" age="23"></Person>
         <Person name = "No one" age="Infinity"></Person>
         <Person name = "NO one again" age="Infinity again">My name</Person> 
       </div>
     );
-  
+  }
 }
 
-/*state = {
-  persons:[
-    {name:'raghav',age:25},
-    {name:'sachin',age:23},
-  ]
-}
-swithNameHandler = () => {
-//this.state.persons[0].name = "Robin" // this is not allowed you can not update the state directly
-this.setState({
-  persons:[
-    {name:'Robin',age:25},
-    {name:'Sachin',age:23}
-  ]
-})
-console.log('It was cliced');
-}
-*/
 // Hoocs are collection of function which you can use in the functional component
-// useSate allows us to manage state in a functional component
 // useState returns an array with exact two element and always 2 elemtn - 1 st elemtn is our current state and the second
 // always be the function which allows us to update this state
 // Earlier we could not manaee
@@ -64,4 +46,4 @@ console.log('It was cliced');
 // on the component file in the special property called the children
 // as you can see the Person tag above will help in importing the compononent and render it with the root
 
-export default app;
+export default App;
