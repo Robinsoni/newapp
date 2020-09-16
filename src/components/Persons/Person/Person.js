0642a3d9c8dc15d5c82ebfedda7c12c23e05886b
robin <sonirobin369@gmail.com>
2020-09-16 09:49:17 +0530

    removing Aux wrapper and using the inbuilt Fragrmant component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Auxiliary from '../../../hoc/Auxiliary'
 //import "./Person.css"
 //import styled from 'styled-components';
@@ -9,11 +9,11 @@ class Person extends Component {
     
     render(){
         return (
-            <Auxiliary>
+            <Fragment>
                 <p onClick={this.props.click}>My name is  {this.props.name} and my age is {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.change} value={this.props.name} />
-            </Auxiliary>
+            </Fragment>
         )
     }
 }
